9485ded5e26a976172960545d03486cf09271047
Valeriy Kozhevnikov <i271980@gmail.com>
2019-10-29 17:32:14 +0300

    SStage XXX

--- latest/docker-entrypoint.sh
+++ latest/docker-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# prevent conflicts with existing
+# Prevent conflicts with existing packages.
 rm -rf node_modules
-
 # Allow run from any user.
 export HOME=/tmp
 # Link global packages.
-/usr/bin/npm link $NPM_PACKAGES
+/usr/bin/npm link susy $NPM_PACKAGES
 
 exec "$@"
